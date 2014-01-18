d80d68052af1a740d7129ca80d088ed49222acc6
Alex Scherb <awscherb@gmail.com>
2014-01-17 21:12:13 -0500

    Reorganization
    Still getting used to using Eclipse, EGit, and the GitHub app for Mac.

---
+++ src/com/awscherb/util/Examples.java
@@ -0,0 +1,92 @@
+package com.awscherb.util;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/** Tests, etc */
+public class Examples {
+    
+    // Stuff
+    static ArrayList<String> ar = new ArrayList<String>();
+    static String val = "line";
+
+    // IO
+    static String file = "line1000000.txt";
+    static String out = "line1000000.txt";
+    static LineReader lr;
+
+    // Start, end time
+    static long s;
+    static long e;
+
+    // Switches
+    static boolean OUT = false;
+    static boolean ARTEST = false;
+    static boolean LRTEST = true;
+
+    // Size for output loop
+    static final int size = 1000000;
+
+    static void out() throws IOException {
+        s = System.currentTimeMillis();
+        BufferedWriter wri = new BufferedWriter(
+                new FileWriter(out, true));
+        try {
+            for (int i = 0; i < size; i = i + 1) {
+                wri.write("line\n"); // Write the line
+            }
+        }         
+        catch (IOException e) {
+            System.out.println("IOException");
+        }
+        finally {
+            wri.close(); 
+            e = System.currentTimeMillis();
+            System.out.println(e - s);
+        }
+    }
+
+    static void initAR() {
+        for (int i = 0; i < size; i = i + 1) {
+            ar.add(val);
+        }
+    }
+
+    public static void main(String[] arg) {
+        if (OUT) {
+            try { out(); } catch (IOException e1) {  }
+        }
+
+        if (ARTEST) { // For comparing a LineReader to an ArrayList
+            s = System.currentTimeMillis();
+            initAR();
+            e = System.currentTimeMillis();
+            System.out.println("ArrayList Initialization: " + (e - s));
+
+            s = System.currentTimeMillis();
+            Iterator<String> arit = ar.iterator();
+            e = System.currentTimeMillis();
+            System.out.println("ArrayList getting iterator: " + (e - s));
+
+            s = System.currentTimeMillis();
+            while(arit.hasNext()) { arit.next(); }
+            e = System.currentTimeMillis();
+            System.out.println("ArrayList Iteration: " + (e - s));
+        }
+
+        if (LRTEST) { // LineReader tests
+            s = System.currentTimeMillis();
+            lr = LineReader.factory(file);
+            e = System.currentTimeMillis();
+            System.out.println("LineReader Initialization: " + (e - s));
+
+            s = System.currentTimeMillis();
+            while(lr.hasNext()) { lr.next(); }
+            e = System.currentTimeMillis();
+            System.out.println("LineReader Iteration: " + (e - s));
+        }
+    }
+
+}
---
+++ src/com/awscherb/util/LineReader.java
@@ -0,0 +1,97 @@
+package com.awscherb.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * LineReader reads a text file one line at a time
+ * @author Alex Scherb
+ * @version 1/17/14
+ */
+public final class LineReader implements Iterator<String>, Iterable<String> {
+
+    /** The input file */
+    private static String input;
+    /** The input file as an <code>ArrayDeque</code>, each line is a string */
+    private ArrayDeque<String> lines;
+    /** The input reader */
+    private BufferedReader br;
+
+    /**
+     * Private constructor; use public static factory method for creation
+     * @param file <code>String</code> location of the file
+     * @throws FileNotFoundException if the file cannot be found
+     */
+    private LineReader(String file) {
+        lines = new ArrayDeque<String>();
+        input = file;
+            try {
+                br = new BufferedReader(new FileReader(input));
+                init();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+    }
+
+    /**
+     * Public static factory method, invokes private constructor
+     * @param s the <code>String</code> file location
+     * @return the new <code>LineReader</code> object
+     */
+    public static LineReader factory(String s) {
+        return new LineReader(s);
+    }
+
+    /**
+     * Add each line of the input file to our <code>ArrayDeque</code>
+     */
+    private void init() {
+        try {
+            String line;
+            while ((line = br.readLine()) != null) { // This is the slowest part
+                lines.push(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Return this; LineReader objects implement <code>Iterator</code> 
+     * @return this
+     */
+    public Iterator<String> iterator() { 
+        return this;
+    }
+
+    /**
+     * Check if this has a next
+     * @return the Boolean result
+     */
+    public boolean hasNext() {
+        return (!lines.isEmpty());
+    }
+
+    /**
+     * Return the next element, if it exists, else throw an exception
+     * @return The next element
+     * @throws NoSuchElementException if we are at the end
+     */
+    public String next() {
+        if (hasNext()) {
+            return lines.pop();
+        }
+        else { throw new NoSuchElementException(); }
+    }
+
+    /** Remove the first element */
+    public void remove() { 
+        lines.pop(); 
+    }
+}
